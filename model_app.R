5ec4337b1db8425223d84ca6d1cece2ac3103022
Emma Robin <emma.robin20@gmail.com>
2023-06-09 19:22:25 -0400

    group26

---
+++ model_app.R
@@ -0,0 +1,217 @@
+library(shiny)
+library(shinydashboard)
+library(visNetwork)
+source("sick_simulation.R")
+shiny::shinyOptions(error = "browser")
+
+# User-Interface 
+ui <- shinydashboard::dashboardPage(
+  shinydashboard::dashboardHeader(title = "Group 26!"),
+  shinydashboard::dashboardSidebar(
+    collapsed = TRUE,
+    shinydashboard::sidebarMenu(
+      sidebarMenu(
+        menuItem("Network", tabName = "network"),
+        menuItem("Monte Carlos", tabName = "monte_carlos"),
+        menuItem("Settings", tabName = "settings")
+      )
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem("network",
+              fluidRow(
+                  shinydashboard::box(
+                    width = 12,
+                    solidHeader = FALSE,
+                    collapsible = FALSE,
+                    collapsed = FALSE,
+                    sliderInput(
+                      inputId = "day_slider",
+                      label = "Day",
+                      min = 1,
+                      max = 20,
+                      value = 1,
+                      width = "100%",
+                      animate = animationOptions(loop = FALSE, interval = 1000)
+                    ),
+                    visNetworkOutput("network_vis"),
+                    actionButton(inputId = "save_data_one", label = "Save Data")
+                  )
+              )
+      ),
+      tabItem("monte_carlos",
+              fluidRow(
+                shinydashboard::box(
+                  width = 4,
+                  solidHeader = TRUE,
+                  collapsible = TRUE,
+                  collapsed = FALSE,
+                  numericInput(inputId = "num_runs",
+                               label = "Number of Runs",
+                               min = 0,
+                               max = 100000,
+                               value = 1000),
+                ),
+                shinydashboard::box(
+                  width = 4,
+                  solidHeader = TRUE,
+                  collapsible = TRUE,
+                  collapsed = FALSE,
+                  actionButton(inputId = "run_monte_carlos",
+                               label = "Run Monte Carlos!")
+                ),
+                shinydashboard::box(
+                  width = 4,
+                  solidHeader = TRUE,
+                  collapsible = TRUE,
+                  collapsed = FALSE,
+                  actionButton(inputId = "save_data",
+                               label = "Save Data")
+                )
+              )
+      ),
+      tabItem("settings",
+              fluidRow(
+                shinydashboard::box(
+                  width = 6,
+                  solidHeader = TRUE,
+                  collapsible = TRUE,
+                  collapsed = FALSE,
+                  sliderInput(
+                    inputId = "infection_rate",
+                    label = "Infection Rate (%)",
+                    min = 1,
+                    max = 100,
+                    value = 2,
+                    step = 1)
+                ),
+                shinydashboard::box(
+                  width = 6,
+                  solidHeader = TRUE,
+                  collapsible = TRUE,
+                  collapsed = FALSE,
+                  numericInput(
+                    inputId = "children_amount",
+                    label = "Number of Children",
+                    min = 1,
+                    max = 1000,
+                    value = 31,
+                    step = 1)
+                )
+              ),
+              fluidRow(
+                shinydashboard::box(
+                  width = 6,
+                  solidHeader = TRUE,
+                  collapsible = TRUE,
+                  collapsed = FALSE,
+                  checkboxInput(inputId = "immunization_on",
+                              label = "Immunizations?",
+                              value = FALSE)
+                ),
+                shinydashboard::box(
+                  width = 6,
+                  solidHeader = TRUE,
+                  collapsible = TRUE,
+                  collapsed = FALSE,
+                  sliderInput(
+                    inputId = "immunization_prob",
+                    label = "Immunization Probability",
+                    min = 1,
+                    max = 100,
+                    value = 50,
+                    step = 1)
+                )
+              )
+      )
+    )
+  )
+)
+
+# 
+server <- function(input, output, session) {
+  
+  simulationOutputs <- reactiveVal(value = data.frame())
+  
+  observeEvent(c(input$infection_rate, 
+                 input$children_amount, 
+                 input$immunization_on,
+                 input$immunization_prob),{
+      simulationOutputs(run_sim(
+        rate = input$infection_rate,
+        children_number = input$children_amount,
+        immunization = input$immunization_on,
+        immunization_rate = input$immunization_prob
+      ))
+  })
+  
+  monteCarlosData <- reactiveVal(value = data.frame())
+  observeEvent(input$run_monte_carlos,{
+    all_data <- data.frame()
+    for(i in 1:input$num_runs){
+      print(i)
+      temp <- simulationOutputs(run_sim(
+        rate = input$infection_rate,
+        children_number = input$children_amount,
+        immunization = input$immunization_on,
+        immunization_rate = input$immunization_prob
+      ))
+      all_data <- rbind(all_data, temp)
+    }
+    monteCarlosData(all_data)
+    shinyalert::shinyalert("Finished!",
+                           "Monte Carlos Run!", 
+                           type = "success")
+  })
+  
+  observeEvent(input$map_slider, {
+    updateSliderInput(
+      session, 'map_slider',
+      value = input$map_slider+1,
+      min = 1,
+      max = simulationOutputs()$day
+    )
+  })
+  
+  update_net_vis <- reactive({
+    req(nrow(simulationOutputs()) > 0)
+    req(input$day_slider)
+    out <- simulationOutputs()[which(
+      simulationOutputs()$day == input$day_slider),]
+    return(out)
+  })
+  
+  observeEvent(update_net_vis(), {
+    temp <- update_net_vis()
+    # browser()
+    if(nrow(temp) > 0){
+      nodes <- getNodes(infected = temp$infected_e)
+      edges <- getEdges(df = temp)
+      
+      output$network_vis <- renderVisNetwork({
+        visNetwork(nodes, edges) %>%
+          visOptions(highlightNearest = TRUE,
+                     selectedBy = "label") # %>%
+          # visOptions(manipulation = TRUE) %>%
+          # visIgraphLayout(layout = "layout_nicely", randomSeed = 1234)
+      })
+    }
+  })
+  
+  observeEvent(input$save_data_one,{
+    write.csv(
+      simulationOutputs(),
+      "data/One Run.csv"
+    )
+  })
+  
+  observeEvent(input$save_data,{
+    write.csv(
+      monteCarlosData(),
+      "data/Monte Carlos.csv"
+    )
+  })
+}
+
+shinyApp(ui, server)
---
+++ sick_simulation.R
@@ -0,0 +1,72 @@
+run_sim <- function(rate, children_number, immunization, immunization_rate){
+  children_temp <- data.frame(
+    "number"          = 1:children_number,
+    "infected_s"      = rep(0, children_number),
+    "infected_e"      = rep(0, children_number),
+    "day"             = rep(1, children_number),
+    "infected_from"   = rep("", children_number),
+    "rate"            = rep(rate, children_number)
+  )
+  
+  children_temp$infected_s[which(children_temp$number == 1)] <- 1
+  children_temp$infected_e[which(children_temp$number == 1)] <- 1
+  
+  if(immunization){
+    for(i in 2:nrow(children_temp)){
+      if(sample(1:100, 1) <= immunization_rate){
+        children_temp$rate[i] <- 0
+      }
+    }
+  }
+  
+  children <- data.frame()
+  day <- 1
+  # loop until all children are infected
+  while(length(which(children_temp$rate == rate)) != 
+        length(which(children_temp$infected_s == 1))){
+    # loop through each child to the next - if infect can spread
+    for(i in 1:nrow(children_temp)){
+      # can child1 infect child2
+      if(children_temp$infected_s[i] == 0){next}
+      for(j in 1:nrow(children_temp)){
+        # already infected
+        if(children_temp$infected_s[j] == 1){next}
+        # child cannot spread from itself to itself
+        if(children_temp$number[i] == children_temp$number[j]){next}
+        # random number generation from 1-100 with 2 as the threshold of being able to infect
+        if(sample(1:100, 1) <= children_temp$rate[j]){
+          children_temp$infected_e[j] <- 1
+          children_temp$infected_from[j] <- i
+        }
+      }
+    }
+    children <- rbind(children, children_temp)
+    children_temp$infected_s <- children_temp$infected_e
+    day <- day + 1
+    children_temp$day <- day
+  }
+  return(children)
+}
+
+getNodes <- function(infected){
+  nodes <- data.frame(
+    "shape"          = rep("dot", length(infected)),
+    "shadow"         = rep(TRUE, length(infected)),
+    "title"          = ifelse(infected == 1, "Infected", "Not Infected"),
+    "label"          = 1:length(infected),
+    "size"           = rep(8, length(infected)),
+    "borderWidth"    = rep(2, length(infected)),
+    "color"          = ifelse(infected == 1, "red", "gray")
+  )
+  return(nodes)
+}
+
+getEdges <- function(df){
+  df <- df[which(df$infected_from != ""),]
+  edges <- data.frame(
+    "from"     = df$infected_e,
+    "to"       = df$infected_from,
+    "color"    = rep("red", nrow(df))
+  )
+  return(edges)
+}
